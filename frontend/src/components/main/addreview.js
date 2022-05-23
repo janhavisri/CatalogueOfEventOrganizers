ecc6f85bea6cac007d1a28306d8b79a7232f58b5
m-triple-m <triplem656@gmail.com>
2022-05-23 15:20:44 +0530

    ..

--- frontend/src/components/main/addreview.js
+++ frontend/src/components/main/addreview.js
@@ -1,33 +1,27 @@
 import "../../stylesheets/signup.css";
- //import "../../stylesheets/addreview.css";
- import * as React from 'react';
- //import Avatar from '@mui/material/Avatar';
- import Button from '@mui/material/Button';
- import TextField from '@mui/material/TextField';
- import { NavLink, useNavigate, useParams } from "react-router-dom";
- //import Link from '@mui/material/Link';
- import Grid from '@mui/material/Grid';
- import { Formik } from "formik";
- import Swal from "sweetalert2";
- import app_config from "../../config";
- import { useState } from "react";
-    function AddReview({ setOpenAddReview }) {
-        const url = app_config.api_url;
-        const [hero, setHero] = useState("");
-        const [ReviewFile, setReviewFile] = useState("");
-        const navigate = useNavigate();
- 
-   const reviewform = {
- 
-     description: "",
-     
+//import "../../stylesheets/addreview.css";
+import * as React from "react";
+//import Avatar from '@mui/material/Avatar';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
+//import Link from '@mui/material/Link';
+import Grid from "@mui/material/Grid";
+import { Formik } from "formik";
+import Swal from "sweetalert2";
+import app_config from "../../config";
+import { useState } from "react";
+function AddReview({ setOpenAddReview }) {
+  const url = app_config.api_url;
+  const [hero, setHero] = useState("");
+  const [ReviewFile, setReviewFile] = useState("");
+  const navigate = useNavigate();
 
-   };
-   const formSubmit = (values) => {
+  const reviewform = {
+    description: "",
+  };
+  const formSubmit = (values) => {
     console.log(values);
-    values.heroimage = hero;
-    values.file = ReviewFile;
-
     const reqOp = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -39,7 +33,7 @@ import "../../stylesheets/signup.css";
       .then((data) => {
         console.log(data);
 
-        if (data.message == "success") {
+        if (data.message === "success") {
           Swal.fire({
             icon: "success",
             title: "Added Successfully!!",
@@ -48,59 +42,22 @@ import "../../stylesheets/signup.css";
       });
   };
 
-  const uploadheroimage = (event) => {
-    const formdata = new FormData();
-    formdata.append("file", event.target.files[0]);
-
-    const reqOptions = {
-      method: "POST",
-      body: formdata,
-    };
-
-    fetch(url + "util/addfile", reqOptions)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setHero(event.target.files[0].name);
-      });
-  };
-
-  const uploadfile = (event) => {
-    const formdata = new FormData();
-    formdata.append("file", event.target.files[0]);
-
-    const reqOptions = {
-      method: "POST",
-      body: formdata,
-    };
-
-    fetch(url + "util/createfile", reqOptions)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setReviewFile(event.target.files[0].name);
-      });
-  };
-
-   return (
-     
-         <div>
-
-         <div class="login">
-         <div class="login-box">
-	{/* <h1>Login</h1> */}
-   <Formik initialValues={reviewform} onSubmit={formSubmit}>
-                 {({ values, handleChange, handleSubmit }) => (
-                   <form onSubmit={handleSubmit}>
-           {/* <Avatar sx={{ m: 5,mx:40, bgcolor: 'secondary.main' }}> */}
-             {/* <LockOutlinedIcon /> */}
-           {/* </Avatar> */}
-           <h1 className="add"> Review</h1>
-           
+  return (
+    <div>
+      <div class="login">
+        <div class="login-box">
+          {/* <h1>Login</h1> */}
+          <Formik initialValues={reviewform} onSubmit={formSubmit}>
+            {({ values, handleChange, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                {/* <Avatar sx={{ m: 5,mx:40, bgcolor: 'secondary.main' }}> */}
+                {/* <LockOutlinedIcon /> */}
+                {/* </Avatar> */}
+                <h1 className="add"> Review</h1>
 
                 <TextField
-                 multiline
-                 rows={5}
+                  multiline
+                  rows={5}
                   className="w-100 mt-4 bg-white"
                   label="Description"
                   variant="filled"
@@ -108,31 +65,25 @@ import "../../stylesheets/signup.css";
                   onChange={handleChange}
                   value={values.description}
                 ></TextField>
-                
-             <Button
-             
-             fullWidth
-               type="submit"
-               variant="contained"
-               sx={{  mt: 4, mb: 2 }}
-               onClick={e=>navigate("/main/browsereview/" )} 
-             >
-               Add
-             </Button>
-             <Grid container justifyContent="flex-end">
-               <Grid item mx={27}>
-                 
-               </Grid>
-             </Grid>
-             </form>
-                 )}
-                 </Formik>
 
-        
-       </div>
-       </div>
-       </div>
-
- );
- }
- export default AddReview;
+                <Button
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  sx={{ mt: 4, mb: 2 }}
+                  onClick={(e) => navigate("/main/browsereview/")}
+                >
+                  Add
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item mx={27}></Grid>
+                </Grid>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AddReview;
